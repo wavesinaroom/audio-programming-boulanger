c2e9674918e1009381b8479c61777b2d7794cd93
Waves in a room <jaureguij@javeriana.edu.co>
2022-12-18 11:26:32 -0500

    [Feature] Add exercise 1.8.3 solution

---
+++ 1.8/exercise_1.8.3/expand.c
@@ -0,0 +1,58 @@
+/* expad.c generate exponential attack or decay breakpoint data*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+typedef struct breakpoint{
+  float time;
+  float value;
+}BREAKPOINT;
+
+int main(int argc, char** argv){
+  int i, npoints;
+  double T,k,a;
+  double ystart,yend;
+  double dur,thisval,thisstep;
+
+  if (argc !=6) {
+    fprintf(stderr, "insuficient arguments. \n");fprintf(stderr, "Usage: expad duration npoints startval endval T\n");
+    return 1;
+  }
+  dur = atof(argv[1]);
+  if (dur<=0.0) {
+    fprintf(stderr,"Error: duration must be positive \n");
+    return 1;
+  }
+  npoints = atoi(argv[2]);
+  if (npoints<=0) {
+    fprintf(stderr, "Error: npoints must be positive \n");
+    return 1;
+  }
+  ystart = atof(argv[3]);
+  yend = atof(argv[4]);
+  if (yend == ystart) {
+    fprintf(stderr, "Warning: start and end values are the same! \n");
+  }
+
+  T = atof(argv[5]);
+  k = dur/npoints; /*npoints = "sample rate" */
+  a = exp(-k/T); 
+
+  thisstep = 0.0;
+  thisval = ystart;
+  fprintf(stderr, "a = %.6lf\n",a);
+  /* make normalized curve, scale output to
+   * input values and range*/
+  for (i=0; i<npoints; ++i) {
+    printf("%.4lf\t%.8lf\n",thisstep,thisval);
+    thisval *= a + ((1.0-a) * yend);
+    thisstep += k;
+  }
+
+  /* print final value */
+    printf("%.4lf\t%.8lf\n",thisstep,thisval);
+
+    fprintf(stderr, "done\n");
+    return 0;
+}
